d954ecf2307822f81c4326eaabcff2cb8e9cc716
Isaac <isaac.brasil@escolarmanager.com.br>
2022-10-31 13:40:08 -0300

    Alterando estilizações

---
+++ Migrations/20221031141050_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCAlunos.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aluno",
+                columns: table => new
+                {
+                    Matricula = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Sexo = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aluno", x => x.Matricula);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aluno");
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCAlunos.Models
+{
+    public enum EnumeradorSexo
+    {
+        Masculino = 0,
+        Feminino = 1
+    }
+    public class Aluno
+    {
+        [Key]
+        public int Matricula { get; set; }
+        public string? Nome { get; set; }
+        public string? Cpf { get; set; }
+
+
+        [DataType(DataType.Date)]
+        public DateTime Nascimento { get; set; }
+        public EnumeradorSexo? Sexo { get; set; }
+    }
+}
